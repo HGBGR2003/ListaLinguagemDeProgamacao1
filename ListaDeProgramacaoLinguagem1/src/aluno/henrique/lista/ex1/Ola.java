d29747e10386edf02699df67a9c9797d7e8d7438
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-05-05 21:35:51 -0300

    Atualizando

--- ListaDeProgramacaoLinguagem1/src/aluno/henrique/lista/ex1/Ola.java
+++ ListaDeProgramacaoLinguagem1/src/aluno/henrique/lista/ex1/Ola.java
@@ -3,8 +3,7 @@ package aluno.henrique.lista.ex1;
 public class Ola {
 
 	public static void main(String[] args) {
-		System.out.println("Comeco do projeto "
-				+ "da lista de linguagem de Programação 1");
+		System.out.println("Olá Mundo!");
 	}
 
 }
