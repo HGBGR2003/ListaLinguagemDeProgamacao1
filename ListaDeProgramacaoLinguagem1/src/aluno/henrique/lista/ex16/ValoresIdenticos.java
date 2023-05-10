b70321dc2cd54287481338e59527067b5bdce5d1
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-05-10 14:38:11 -0300

    Exercicio Finalizado

---
+++ ListaDeProgramacaoLinguagem1/src/aluno/henrique/lista/ex16/ValoresIdenticos.java
@@ -0,0 +1,43 @@
+package aluno.henrique.lista.ex16;
+
+import javax.swing.JOptionPane;
+
+public class ValoresIdenticos {
+
+	public static void main(String[] args) {
+		int[] v1;
+		int[] v2;
+		int valores1;
+		int valores2;
+		int contador = 0;
+		int usuarioDigitou;
+		
+		usuarioDigitou = Integer.parseInt(JOptionPane.showInputDialog("Entre com tamanho do vetor de até 50 :"));
+		v1 = new int[usuarioDigitou];
+		v2 = new int[usuarioDigitou];
+		
+		for(int i = 0; i < usuarioDigitou;i++) {
+		  valores1 = Integer.parseInt(JOptionPane.showInputDialog("Entre com os valores para o primeiro vetor:"));	  
+		  v1[i] = valores1;
+		}
+	
+		for(int i = 0; i < usuarioDigitou;i++) {
+			valores2 = Integer.parseInt(JOptionPane.showInputDialog("Entre com os valores do segundo vetor:"));
+			v2[i] = valores2;
+		}
+		
+		for(int i = 0; i < usuarioDigitou; i++) {
+			if(v1[i] == v2[i]) {
+				contador++;
+			}
+		}
+		
+		if(contador == 0) {
+			System.out.println("V1 e V2 não possuem valores idênticos nas mesmas posições.");
+		}else {
+			System.out.println("V1 e V2 possuem " + contador + " valores idênticos nas mesmas posições.");
+		}
+		
+	}
+
+}
