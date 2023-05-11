21f1aa0f3de6396c40b0ce8dee6fdf4453361cb0
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-05-11 10:32:14 -0300

    Exercicio Finalizado

---
+++ ListaDeProgramacaoLinguagem1/src/aluno/henrique/lista/ex17/ValoresSorteados.java
@@ -0,0 +1,29 @@
+package aluno.henrique.lista.ex17;
+
+import javax.swing.JOptionPane;
+
+public class ValoresSorteados {
+
+	public static void main(String[] args) {
+		int numeroAleatorio = (int) Math.floor(Math.random() * 100) + 1;
+		int tentativaUsuario = 0;
+		int contador = 0;
+		
+		while(numeroAleatorio != tentativaUsuario) {
+			tentativaUsuario = Integer.parseInt(JOptionPane.showInputDialog("Entre com um número de 0 á 100, e tente acertar junto com a máquina:"));
+			if (numeroAleatorio > tentativaUsuario) {
+				contador++;
+				System.out.println("O número informado é Menor que o valor sorteado");
+			}
+			
+			if (numeroAleatorio < tentativaUsuario) {
+				contador++;
+				System.out.println("O número informado é Maior que o valor sorteado");
+			}
+		}
+		
+		JOptionPane.showMessageDialog(null, "Parabéns você acertou!!, A quantidade de tentativas foi de " + contador);
+
+		JOptionPane.showMessageDialog(null, "O número sorteado foi: " + numeroAleatorio);
+	}
+}
