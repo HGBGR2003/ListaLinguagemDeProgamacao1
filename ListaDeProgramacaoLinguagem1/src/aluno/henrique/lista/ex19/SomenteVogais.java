c6233a08ebf8a65bde53683d59a76329658c8ce9
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-05-12 10:58:27 -0300

    Exercicio Finalizado

---
+++ ListaDeProgramacaoLinguagem1/src/aluno/henrique/lista/ex19/SomenteVogais.java
@@ -0,0 +1,25 @@
+package aluno.henrique.lista.ex19;
+
+import javax.swing.JOptionPane;
+
+public class SomenteVogais {
+
+	public static void main(String[] args) {
+		String frase;
+		
+		frase = JOptionPane.showInputDialog("Entre com uma frase:");
+				
+		for(int i = 0; i < frase.length(); i++) {
+			char vogais = frase.toLowerCase().charAt(i);
+			
+			if (vogais == 'a' || vogais == 'e' || vogais == 'i' || vogais == 'o' || vogais == 'u') {
+				System.out.println("As vogais da frase : " + vogais + " ");
+			}
+			
+		}
+	}
+
+
+}
+
+
