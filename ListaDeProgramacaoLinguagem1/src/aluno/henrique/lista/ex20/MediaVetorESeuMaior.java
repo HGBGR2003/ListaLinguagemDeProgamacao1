c01b3268cca4c0f560fe43951b102fb2993f22db
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-05-12 11:27:42 -0300

    Finalizado minha parte

---
+++ ListaDeProgramacaoLinguagem1/src/aluno/henrique/lista/ex20/MediaVetorESeuMaior.java
@@ -0,0 +1,34 @@
+package aluno.henrique.lista.ex20;
+
+import javax.swing.JOptionPane;
+
+public class MediaVetorESeuMaior {
+
+	public static void main(String[] args) {
+		int[] vetorPrincipal;
+		int valorUsuario;
+		int valorContador;
+		int somatorio = 0;
+		int maior = 0;
+		double mediaVetor = 0;
+		
+		valorContador = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade de números dentro do array:"));
+		vetorPrincipal = new int [valorContador];
+		
+		for(int i = 0; i < vetorPrincipal.length; i++) {
+			valorUsuario = Integer.parseInt(JOptionPane.showInputDialog("Entre com um número positivo:"));
+			vetorPrincipal[i] = valorUsuario;
+			somatorio = somatorio + vetorPrincipal[i];
+			mediaVetor = somatorio / vetorPrincipal.length;
+			if (vetorPrincipal[i] > maior ) {
+				maior = vetorPrincipal[i];
+			}
+		}
+		
+		System.out.println("O somatório total é " + somatorio);
+		System.out.println("A media é " + mediaVetor);	
+		System.out.println("O maior valor no vetor é " + maior);
+		
+	}
+
+}
