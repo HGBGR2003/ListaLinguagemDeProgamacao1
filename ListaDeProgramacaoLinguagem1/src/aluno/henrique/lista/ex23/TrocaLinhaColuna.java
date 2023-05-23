1db8b5f00101ae0927a0c5f22997404f22b64dbc
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-05-23 17:03:57 -0300

    Voltando

---
+++ ListaDeProgramacaoLinguagem1/src/aluno/henrique/lista/ex23/TrocaLinhaColuna.java
@@ -0,0 +1,18 @@
+package aluno.henrique.lista.ex23;
+
+import javax.swing.JOptionPane;
+
+public class TrocaLinhaColuna {
+	public static void main(String[] args) {
+		int [][] vetorPrincipal;
+		int digitadoPeloUsuario;
+		
+		digitadoPeloUsuario = Integer.parseInt(JOptionPane.showInputDialog("Informe o tamanho do array?"));
+		vetorPrincipal = new int[digitadoPeloUsuario][digitadoPeloUsuario];
+		
+		for(int l = 0; l < vetorPrincipal.length; l++) {
+			
+		}
+	}
+
+}
