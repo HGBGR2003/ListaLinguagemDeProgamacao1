8170448e7b4c9fb57ff5bf7a57a440fadeed3fa1
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-05-24 20:10:58 -0300

    Atualizando

---
+++ ListaDeProgramacaoLinguagem1/src/aluno/henrique/lista/ex24/ConfirmacaoDeVerdadeiroOuFalso.java
@@ -0,0 +1,32 @@
+package aluno.henrique.lista.ex24;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class ConfirmacaoDeVerdadeiroOuFalso {
+
+	public static void main(String[] args) {
+		boolean [][] principal;
+		ConfirmacaoDeVerdadeiroOuFalso cvf = new ConfirmacaoDeVerdadeiroOuFalso();
+		int tamanhoDoVet;
+		boolean verdOuFal;
+		
+		tamanhoDoVet = Integer.parseInt(JOptionPane.showInputDialog("Entre com o tamanho do vetor:"));
+		
+		principal = new boolean [tamanhoDoVet][tamanhoDoVet];
+		
+		for(int l = 0; l < principal.length; l++) {
+			for(int c = 0; c < principal.length; c++) {
+				verdOuFal = Boolean.parseBoolean
+						(JOptionPane.showInputDialog("Entre com um valor true ou false:"));
+				principal[l][c] = verdOuFal; 
+			}
+		}
+		
+		System.out.println("Matriz Normal");
+		
+		
+	}
+
+}
